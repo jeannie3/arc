ea5b468dd9d5df5b2d4c37e65c483b2d5250e82f
Jeannie <jeanniele2011@gmail.com>
2019-08-30 12:48:39 -0400

    Created Scenario Detail Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScenarioDetailComponent } from './components/scenario-detail/scenario-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScenarioDetailComponent
   ],
   imports: [
     BrowserModule,
