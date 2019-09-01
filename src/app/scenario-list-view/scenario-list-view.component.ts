31e7bf16d0d39f48432bca2a2cbbd3e814cbe4b6
Hao Dang <hdang32@gatech.edu>
2019-08-31 22:32:10 -0400

    mock scenario

--- src/app/scenario-list-view/scenario-list-view.component.ts
+++ src/app/scenario-list-view/scenario-list-view.component.ts
@@ -1,28 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
-import { Scenario } from './scenario';
+import { Scenario, mockScenarios } from './scenario';
 @Component({
   selector: 'app-scenario-list-view',
   templateUrl: './scenario-list-view.component.html',
   styleUrls: ['./scenario-list-view.component.scss']
 })
 export class ScenarioListViewComponent implements OnInit {
-  scenario: Scenario = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  scenarios: Scenario[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+  scenarios: Scenario[] = mockScenarios;
 
   constructor() { }
 
