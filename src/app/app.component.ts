c939c299cad3bc1b30ac299bbaa9715f09069a92
SaiGundlapalli <sgundlapalli@gatech.edu>
2019-08-31 21:27:43 -0400

    clicking on answer choice will update current scene

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { scenarios } from './scenarios';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arc';
+  scenarioList = scenarios;
 }
--- src/app/components/scene-container/scene-container.component.ts
+++ src/app/components/scene-container/scene-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { scenarios } from '../../scenarios';
 import { Scene } from '../../scene';
 
@@ -8,10 +8,13 @@ import { Scene } from '../../scene';
   styleUrls: ['./scene-container.component.scss']
 })
 export class SceneContainerComponent implements OnInit {
-  currentScene: Scene = scenarios[0].scenes[0];
+  @Input() currentScene: Scene;
+  //currentScene: Scene = scenarios[0].scenes[0];
 
-  onAnswered(nextScene: number) {
+  updateScene(nextScene: number) {
     console.log("The next scene is " + nextScene);
+    this.currentScene = (scenarios[0].scenes).find(scene => scene.id == nextScene);
+    //this.ngOnInit();
   }
 
   constructor() { }
