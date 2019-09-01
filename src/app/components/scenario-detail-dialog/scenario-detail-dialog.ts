ae951c3bcdc1a11c22c11ef02b498a98d0748867
Jeannie <jeanniele2011@gmail.com>
2019-09-01 12:33:27 -0400

    Added testScenario button and scenes

--- src/app/components/scenario-detail-dialog/scenario-detail-dialog.ts
+++ src/app/components/scenario-detail-dialog/scenario-detail-dialog.ts
@@ -1,62 +1,58 @@
-import {Component, Inject} from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Scenario, SCENARIOS } from 'src/app/mock/mock-scenarios';
-import { FormGroup, FormControl, Validators, FormControlName } from '@angular/forms';
-const Fs = require('fs');
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'app-scenario-detail-dialog',
-    templateUrl: 'scenario-detail-dialog.html',
-    styleUrls: ['./scenario-detail-dialog.scss']
-  })
-  export class ScenarioDetailDialogComponent {
+  selector: 'app-scenario-detail-dialog',
+  templateUrl: 'scenario-detail-dialog.html',
+  styleUrls: ['./scenario-detail-dialog.scss']
+})
+export class ScenarioDetailDialogComponent {
 
-    public scenario: Scenario;
-    public formGroup: FormGroup;
+  public scenario: Scenario;
+  public formGroup: FormGroup;
 
-    constructor(
-      public dialogRef: MatDialogRef<ScenarioDetailDialogComponent>,
-      @Inject(MAT_DIALOG_DATA) public data) {
-        this.scenario = SCENARIOS.find(scenario => scenario.id === data.id);
+  constructor(
+    public dialogRef: MatDialogRef<ScenarioDetailDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data) {
+      this.scenario = SCENARIOS.find(scenario => scenario.id === data.id);
 
-        this.formGroup = new FormGroup({
-          title: new FormControl(this.scenario.title, Validators.required),
-          description: new FormControl(this.scenario.description)
-        });
-
-
-        this.formGroup.valueChanges.subscribe(value => {
-          // only to see value for now - delete afterwards
-          console.log(value);
-        });
+      if (!this.scenario) {
+        this.scenario = {
+          id: 0, // fake id for now - TODO: should generate a new id
+          title: '',
+          description: '',
+          sceneIds: []
+        };
       }
 
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-
-    onSave() {
-      console.log('save data here. Get form group value and post to url/write to file here');
-
-      const data = this.formGroup.value;
-      const path = '../../mock/mock-scenarios.ts';
-
-      const json = JSON.stringify(data, null, 2);
-
-      Fs.writeFile(path, json, (err) => {
-        if (err) {
-          console.error(err);
-          throw err;
-        }
-
-        console.log('Saved data to file.');
+      this.formGroup = new FormGroup({
+        title: new FormControl(this.scenario.title, Validators.required),
+        description: new FormControl(this.scenario.description)
       });
-      
-      this.dialogRef.close();
+
+
+      this.formGroup.valueChanges.subscribe(value => {
+        // only to see value for now - delete afterwards
+        console.log(value);
+      });
     }
 
-    onCancel() {
-      this.dialogRef.close();
-    }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
+  onSave() {
+    console.log('save data here. Get form group value and post to url/write to file here');
+    this.dialogRef.close();
+  }
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  testScenario() {
+    console.log('test scenario here');
+  }
 }
