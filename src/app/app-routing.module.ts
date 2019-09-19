04142847736096d902c0175d9728400643c61b0b
Vijay <vjnambi@gmail.com>
2019-09-18 21:33:31 -0400

    Create role-list-view component and tried routing
    Used CLI to generate new component and modified app-routing.module.ts to try to get routing to work but it doesn't

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { ScenarioDetailComponent } from './components/scenario-detail/scenario-detail.component';
 import { ScenarioListViewComponent } from './components/scenario-list-view/scenario-list-view.component';
 import { SceneContainerComponent } from './components/scene-container/scene-container.component';
+import { RoleListViewComponent } from './components/role-list-view/role-list-view.component';
 
 const routes: Routes = [
   { path: '', component: ScenarioListViewComponent },
   { path: 'scenario/:id', component: ScenarioDetailComponent},
   { path: 'scene', component: SceneContainerComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+  { path: 'role', component: RoleListViewComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/role-list-view/role-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-role-list-view',
+  templateUrl: './role-list-view.component.html',
+  styleUrls: ['./role-list-view.component.scss']
+})
+export class RoleListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
