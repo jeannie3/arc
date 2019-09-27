37fc570aaee7361f1fc36b8d79ea4d99d535c8f4
Jeannie <31482326+jeannie3@users.noreply.github.com>
2019-09-27 16:13:10 -0400

    Add service layer (#38)
    * Added service layer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
 
-import { ScenarioDetailComponent } from './components/scenario-detail/scenario-detail.component';
-import { ScenarioListViewComponent } from './components/scenario-list-view/scenario-list-view.component';
+import { NgModule } from '@angular/core';
 import { SceneContainerComponent } from './components/scene-container/scene-container.component';
 
 const routes: Routes = [
-  { path: '', component: ScenarioListViewComponent },
-  { path: 'scenario/:id', component: ScenarioDetailComponent},
+  // { path: '', component: ScenarioListViewComponent },
+  // { path: 'scenario/:id', component: ScenarioDetailComponent},
   { path: 'scene', component: SceneContainerComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'scene' }
 ];
 
 @NgModule({
