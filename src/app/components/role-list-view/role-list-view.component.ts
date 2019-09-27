ec865ff17df6902af942697630572f6612dc51a8
Vijay <vjnambi@gmail.com>
2019-09-27 12:10:46 -0400

    Completed Choose a Role
    Vijay figured out how his brain cells work and created the "Role" Model

--- src/app/components/role-list-view/role-list-view.component.ts
+++ src/app/components/role-list-view/role-list-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { Role } from 'src/app/models/role';
+
 
 @Component({
   selector: 'app-role-list-view',
@@ -7,15 +10,26 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./role-list-view.component.scss']
 })
 export class RoleListViewComponent implements OnInit {
-  roles = ['role1','role2','role3','role4'];
+  formRoles: FormArray;
+  stuff = ['/scene','','/scenario/new']
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  this.formRoles = new FormArray([]);
+  for(var i = 1; i < 4; i++){
+  this.formRoles.push(this.formBuilder.group({
+        id: '0',
+        title: 'Role' + i,
+        link: this.stuff[i-1]
+  }));
   }
+  }
+
     
-  chooseRole(){
-      this.router.navigate(['scene']);
+  chooseRole(role: Role){
+      console.log(role);
+      this.router.navigate([role.link]);
   }
 
 }
---
+++ src/app/models/role.ts
@@ -0,0 +1,5 @@
+export class Role {
+  id: string;
+  title: string;
+  link: string;
+}
