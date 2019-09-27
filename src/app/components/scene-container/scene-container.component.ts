a9f9154c75f8fb053dfb4303fa9d3b838d73f10d
Sai Gundlapalli <sai.g1996@gmail.com>
2019-09-27 16:09:15 -0400

    fetch scenes and answer choices for db to be able to navigate

--- src/app/components/scene-container/scene-container.component.ts
+++ src/app/components/scene-container/scene-container.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Scene, SceneType } from '../../models/scene';
 import { scenes } from 'src/app/mock/scenes';
+import { AnswerChoice } from 'src/app/models/answer-choice';
+import { ScenarioService} from '../../services/scenario.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-scene-container',
@@ -10,20 +13,27 @@ import { scenes } from 'src/app/mock/scenes';
 })
 export class SceneContainerComponent implements OnInit {
   currentScene: Scene; 
+  answerChoices: AnswerChoice[]
 
   updateScene(nextScene: string) {
     console.log("The next scene is " + nextScene);
 
     // if the next scene is -1, the current scene is the last scene
     this.currentScene = (scenes).find(scene => scene.id == nextScene);
-    if(this.currentScene.type == SceneType.Feedback) {
+    //this.answerChoices = this.scenarioService.getAnswerChoices(this.currentScene.id);
+    console.log(this.answerChoices);
+    if (this.currentScene.type == SceneType.Feedback) {
       this.router.navigate(['/explanation/' + nextScene]);
     }
   }
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private scenarioService: ScenarioService) {
     // this.scenarios = scenarioService.getScenario('1');
     this.currentScene = scenes[0];
+    scenarioService.getAnswerChoices(this.currentScene.id).subscribe(value => {
+      console.log(value);
+      this.answerChoices = value;
+    });
   }
 
   ngOnInit() { }
