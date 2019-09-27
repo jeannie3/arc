217debc714cddbfddeda20693faaff3ed691c101
sachanta6 <42822042+sachanta6@users.noreply.github.com>
2019-09-27 19:01:19 -0400

    Account creation (#41)
    Added log in and registration functionalities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { SceneContainerComponent } from './components/scene-container/scene-container.component';
 import { RoleListViewComponent } from './components/role-list-view/role-list-view.component';
 
+
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+
   // { path: '', component: ScenarioListViewComponent },
   // { path: 'scenario/:id', component: ScenarioDetailComponent},
   { path: 'scene', component: SceneContainerComponent },
   { path: 'role', component: RoleListViewComponent},
-  { path: '**', redirectTo: 'scene' }
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+
 import { ScenarioDetailComponent } from './components/scenario-detail/scenario-detail.component';
 import { ScenarioListViewComponent } from './components/scenario-list-view/scenario-list-view.component';
 import { ScenarioSceneContextComponent } from './components/scenario-scene-context/scenario-scene-context.component';
@@ -24,6 +30,8 @@ import { RoleListViewComponent } from './components/role-list-view/role-list-vie
     SceneContainerComponent,
     ScenarioDetailComponent,
     ScenarioListViewComponent,
+    LoginComponent,
+    RegisterComponent,
     RoleListViewComponent,
   ],
   imports: [
@@ -34,16 +42,14 @@ import { RoleListViewComponent } from './components/role-list-view/role-list-vie
     ReactiveFormsModule,
     FormsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [
-
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
