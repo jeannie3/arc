4b47420a07d95a8142f4149927bafd37b347846e
Vijay <vjnambi@gmail.com>
2019-10-16 10:24:49 -0400

    Changed username to email and made it case-insensitive. Also added confirm password field on registration. Comments are in anticipation of Jeannie's PR or my failed attempt at cross-field form validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,7 +3,9 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
-
+import { matchPassword } from '../validators/matchPassword';
+//import { ErrorMessageDialogComponent } from './components/error-message-dialog/error-message-dialog.component';
+//import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     templateUrl: 'register.component.html',
@@ -18,15 +20,20 @@ export class RegisterComponent implements OnInit {
         private formBuilder: FormBuilder,
         private router: Router,
         private authenticationService: AuthService,
+        //private dialog: MatDialog
     ) {
     }
 
     ngOnInit() {
         this.registerForm = this.formBuilder.group({
             firstName: ['', Validators.required],
-            username: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(6)]]
-        });
+            email: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirm_password: ['', [Validators.required, Validators.minLength(6)]]
+        }
+        //,matchPassword
+        );
+        
     }
 
     // convenience getter for easy access to form fields
@@ -36,20 +43,28 @@ export class RegisterComponent implements OnInit {
         this.submitted = true;
 
         // stop here if form is invalid
-        if (this.registerForm.invalid) {
+        if (this.registerForm.invalid || this.f.password.value != this.f.confirm_password.value) {
             return;
         }
 
         this.loading = true;
-        this.authenticationService.register(this.f.firstName.value, this.f.username.value, this.f.password.value)
+        this.authenticationService.register(this.f.firstName.value, this.f.email.value.toLowerCase(), this.f.password.value)
             .pipe(first())
             .subscribe(
                 data => {
-                    this.router.navigate(['/login']);
+                    this.router.navigate(['/role']);
                 },
                 error => {
                     this.loading = false;
                 });
+        window.alert("account successfully created")
+        /**
+        this.dialog.open(ErrorMessageDialogComponent, {
+        data: {
+          errorMessage: "Successful Account Creation"
+        }
+        });
+        */
     }
 
     cancel() {
