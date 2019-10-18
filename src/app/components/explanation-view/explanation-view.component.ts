c498ac95d8421e0fd93e718618b5c000a5f93f93
Sai Gundlapalli <sai.g1996@gmail.com>
2019-10-18 14:09:47 -0400

    Enhance feedback (#60)
    * enhance feedback view

--- src/app/components/explanation-view/explanation-view.component.ts
+++ src/app/components/explanation-view/explanation-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MatStepperModule} from '@angular/material/stepper'; 
-import {Scene} from '../../models/scene';
+import {Scene, SceneType} from '../../models/scene';
 
 import { Router, ActivatedRoute } from '@angular/router';
 import { ScenarioService } from 'src/app/services/scenario.service';
@@ -13,19 +13,20 @@ import { ScenarioService } from 'src/app/services/scenario.service';
   styleUrls: ['./explanation-view.component.scss']
 })
 export class ExplanationViewComponent implements OnInit {
-  id;
-  currentScene;
-  scenes;
-  roleId;
+  sceneId: string;
+  currentScene: Scene;
+  scenes: Array<Scene>;
+  roleId: string;
   success: boolean;
+  firstSceneId: string;
 
   constructor(private router: Router, private _Activatedroute:ActivatedRoute, private scenarioService: ScenarioService) {
     this._Activatedroute.paramMap.subscribe(params => { 
-      this.id = params.get('sceneId');
+      this.sceneId = params.get('sceneId');
       this.roleId = params.get('roleId');
       scenarioService.getScenes(this.roleId).subscribe(scenes => {
-        this.currentScene = scenes.find(scene => scene.id == this.id);
-        if (this.currentScene.endingMessage == "Congrats!") {
+        this.currentScene = scenes.find(scene => scene.id == this.sceneId);
+        if (this.currentScene.type == SceneType.FB_POSITIVE ) {
           this.success = true;
         } else {
           this.success = false;
@@ -38,6 +39,22 @@ export class ExplanationViewComponent implements OnInit {
   }
 
   tryAgain() {
+    // hard coded scenario id here, will need to modify when we add multiple
+    this.scenarioService.getRoles("1").subscribe( roles => {
+      roles.forEach(role => {
+        if (role.id == this.roleId) {
+          this.firstSceneId = role.first_scene_id;
+        }
+      });
+
+      if (this.firstSceneId) {
+        this.router.navigate([this.roleId,'scene',this.firstSceneId]);
+      } else {
+        console.log("For some reason the first scene for the given role was not found");
+      }
+    });
+  }
+  goToRoles() {
     this.router.navigate(['/role']);
   }
 }
