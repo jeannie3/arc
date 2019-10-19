cd2b19e6565b36bd4ff30666057d1afc39564a48
Jeannie <jeanniele2011@gmail.com>
2019-10-18 21:00:49 -0400

    Fixed styling and typo

--- src/app/error-state-matchers/password-error-state-matcher.ts
+++ src/app/error-state-matchers/password-error-state-matcher.ts
@@ -1,7 +1,7 @@
 import { ErrorStateMatcher } from '@angular/material/core';
 import { FormControl } from '@angular/forms';
 
-/** Marks confirm_email mat-input as invalid if email and confirm_email do not match */
+/** Marks confirm_password mat-input as invalid if password and confirm_password do not match */
 export class PasswordErrorStateMatcher implements ErrorStateMatcher {
     isErrorState(control: FormControl): boolean {
         if (!control.touched) {
